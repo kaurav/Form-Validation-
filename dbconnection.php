b85f799e6290052cdd3c5ac4fc025d79b610c076
kaurav <kauravi420@gmail.com>
2016-07-09 21:34:55 +0530

    email validation and database connection

---
+++ dbconnection.php
@@ -0,0 +1,28 @@
+<?php 
+$server = "localhost";
+$user = "root";
+$password = "avi";
+$dbname = "use zabiusassignment";
+
+$connection = new mysqli($server, $user, $password);
+
+if($connection->connect_error){
+die("connection failed". $connection->connect_error);
+}
+echo "connected successfully";
+
+if($connection->query($dbname) === TRUE) {
+  echo "DB selected ";
+}else{
+echo "error: while selecting db: ". $connection->error;
+}
+$name='avi';
+$lastname = 'kaur';
+$email = 'avikaur@gmail.com';
+$mobile = '35342';
+$gender = 'm';
+mysqli_query($connection,"INSERT INTO user set name = '$name', lastname = '$lastname', email = '$email' , mobile = '$mobile', gender = '$gender'");
+
+
+$connection->close();
+?>

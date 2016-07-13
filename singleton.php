66ab75b07cc784cf573463e6204239721c6b5269
kaurav <kauravi420@gmail.com>
2016-07-14 01:16:51 +0530

    made changes in singleton

--- singleton.php
+++ singleton.php
@@ -4,23 +4,38 @@
 class Singleton
 {
   private static $instance = null;
-
+  private static $call = 0;
   public static function sing_funtion()
   {
+
+  self :: $call++;
     if(self::$instance==null)
     {
-      echo "hello ";
+
+ echo self :: $call;
+      echo "hello <br>";
       self::$instance = new Singleton;
-//      print_r(self::$instance); die();
     }
-print_r(self::$instance);die();
+
     return self::$instance;
   }
+
+  public function checkit(){
+echo self :: $call;
+}
+
 }
 
 $obj1 = Singleton::sing_funtion();
-$obj1 = Singleton::sing_funtion();
-$obj1 = Singleton::sing_funtion();
+$obj1->checkit().'<br>';
+$obj2 = Singleton::sing_funtion();
+$obj2->checkit().'<br>';
+$obj3 = Singleton::sing_funtion();
+$obj3->checkit().'<br>';
+
+//$obj1->checkit().'<br>';
+//$obj2->checkit().'<br>';
+//$obj3->checkit();
 ?>
 </body>
 </html>

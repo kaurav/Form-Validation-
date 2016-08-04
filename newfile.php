2af28b1fc5d894dc35e86694ead2b24a4f743f57
kaurav <kauravi420@gmail.com>
2016-08-04 22:22:38 +0530

    form builder with bootstrap

--- newfile.php
+++ newfile.php
@@ -1,79 +1,152 @@
 <html>
 <head>
-<script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.4/jquery.min.js"></script>
-<script>
+
+	<title>Bootstrap Example</title>
+	<meta charset="utf-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<link rel="stylesheet" href="/Form-Validation-/bootstrap/css/bootstrap.css">
+	<script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.4/jquery.min.js"></script>
+
+	<script src="/Form-Validation-/bootstrap/js/bootstrap.js"></script>
+
+	<style type="text/css">
+		.container {
+			width: 1020px;	
+		}
+		.col-sm-6{
+/*background-color:black;
+color: white;*/
+}
+.text-center{
+	color: white;
+}
+
+</style>
+
+</head>
+
+<body style="background-color: black;">
+	<div class ="text-center">
+		<h1>Form builder</h1>
+
+	</div>
+
+	<div class="container">
+		<div class="row">
+			<!-- buttons -->
+			<div class="jumbotron">
+				<div class="text-center">
+					<button id="add" class="btn btn-primary btn-lg">ADD</button>
+					<button id="delete" class="btn btn-danger btn-lg">Delete</button>
+					<button id="fetch" class="btn btn-primary btn-lg">Fetch</button>
+					<button id="clear" class="btn btn-default btn-lg">Clear</button>
+					<input type="submit" name="submit" id="submit" class="btn btn-primary btn-lg">
+
+				</div>
+				<br/>
+				<div class="alert alert-info remove">
+					rows: ( <b><span id="counter"></span></b> )
+				</div>
+				<span id="emptyinputfields"></span>
+			
+			<!-- buttons -->
+			<!-- form -->
+			<!-- <div class="jumbotron text-center"> -->
+			<div class="table table-striped">
+				<form id="formid" method="post" action="" ">  
+					<div class="form-group">
+						<table id="tabid" class="table table-condensed table-striped">
+							<thead><tr><th colspan="3">NAME</th> <th>EMAIL</th></tr></thead>
+							<tbody id="tbid">
 
 
-$("document").ready(function(){
 
-	var inc = 0, count = 0;
-	// Add Button Functionality
-	$("#add").on("click",function(e){ 
-		inc++;
-		var name = "name"+inc.toString();
-		var email = "email"+inc.toString();
+							</tbody>
+						</table>
 
-				var rowid = "row"+inc.toString(); // row id that i have assigned here, it will be returned in .each function
-						
+						<!-- <div id="response"></div> -->
+					</div>
+				</form>
+			</div>
+			<!-- form -->
+			<!-- </div> -->
+		</div>
+		</div>    <!-- row -->
 
-		$("#tbid").append("<tr id ='"+rowid+"'> <td>Name</td> <td class = 'common'><input type='text' id='"+name+"' name='"+name+"'/></td><td>Email</td> <td class = 'common'><input type='text' id='"+email+"' name='"+email+"'/></td> </br></tr>");
- 		
-		var total = $('#tbid tr').length; //finding the length of array
-         $('#counter').html(total);
-         e.preventDefault();
+	</div> <!-- container -->
 
-	});
+	<script>
+
+		$("document").ready(function(){
+
+			var inc = 0, count = 0;
+// Add Button Functionality
+$("#add").on("click",function(e){ 
+	inc++;
+	var name = "name"+inc.toString();
+	var email = "email"+inc.toString();
+
+var rowid = "row"+inc.toString(); // row id that i have assigned here, it will be returned in .each function
+
+
+$("#tbid").append("<tr id ='"+rowid+"'> <td>Name</td> <td><input class='form-control' type='text' id='"+name+"' name='"+name+"'/></td> <td>&nbsp;&nbsp;</td>  <td>Email</td> <td><input class='form-control' type='text' id='"+email+"' name='"+email+"'/></td> </br></br></br></tr>");
+
+var total = $('#tbid tr').length; //finding the length of array
+$('#counter').html(total);
+e.preventDefault();
+
+});
 // add ends here...
 
 //delete's functionality goes here...
-	$("#delete").on('click',function(e){
-		$("#tabid tr:last").remove();
-		
-		var total = $('#tbid tr').length; //finding the length of array
-         $('#counter').html(total);
-         e.preventDefault();
-	
-	});
-		
-// Fetch button's functionality
+$("#delete").on('click',function(e){
+	$("#tabid tr:last").remove();
 
-	
-$("#fetch").on("click",function(e){
-			count++;
+var total = $('#tbid tr').length; //finding the length of array
+$('#counter').html(total);
+e.preventDefault();
 
-		
-	      	$.ajax({    
-	        type: "POST",
-	        url: "selectalldata.php",             
-	        success: function(response){  
-
-		var name = "name"+inc.toString();
-		var email = "email"+inc.toString();
-		 	var n = "name"+count.toString(); 
-		  	var name = "name"+count.toString();
-
-	        var arr = response['data']; 
-	        var len = arr.length;
-	        //console.log(len);
-	        if(arr.length > 0){
-	        $("#clear").trigger("click");
-
-           }
-
-           for (var i = 0 ; i < arr.length; i++){
-			
-           	var value = arr[i] 
-           	
-
-			$("#tbid").append("<tr id='"+n+"' class='addresfield'> <td>NAME </td><td><input name= '"+name+"'class='hello' type='text' value = '"+value['Name']+"'/></td><td>Email </td><td><input name='"+email+"' class='hello' type='text' value = '"+value['Email']+"'/></br></td></tr>");
-	
-
-           }
-             //alert(JSON.stringify(arr));
-        }
 });
 
-      		 e.preventDefault();
+// Fetch button's functionality
+
+
+$("#fetch").on("click",function(e){
+	count++;
+
+
+	$.ajax({    
+		type: "POST",
+		url: "selectalldata.php",             
+		success: function(response){  
+
+			var name = "name"+inc.toString();
+			var email = "email"+inc.toString();
+			var n = "name"+count.toString(); 
+			var name = "name"+count.toString();
+
+			var arr = response['data']; 
+			var len = arr.length;
+//console.log(len);
+if(arr.length > 0){
+	$("#clear").trigger("click");
+
+}
+
+for (var i = 0 ; i < arr.length; i++){
+
+	var value = arr[i] 
+
+
+	$("#tbid").append("<tr id='"+n+"' class='addresfield'> <td>NAME </td><td><input class='form-control' name= '"+name+"' type='text' value = '"+value['Name']+"'/></td><td>&nbsp;&nbsp;</td><td>Email </td><td><input class='form-control' name='"+email+"'  type='text' value = '"+value['Email']+"'/></td></br></br></br></tr>");
+
+
+}
+//alert(JSON.stringify(arr));
+}
+});
+
+	e.preventDefault();
 
 
 });
@@ -84,96 +157,101 @@ $("#fetch").on("click",function(e){
 
 // clear starts here...
 $("#clear").on("click", function(e){
-    $("#tabid tr").remove();
-      var total = $('#tbid tr').length;
-      $('#counter').html(total);
-      e.preventDefault();
+	$("#tabid tr, .remove").remove();
+
+	var total = $('#tbid tr').length;
+	$('#counter').html(total);
+	e.preventDefault();
 });
 // clear ends here...
 
 
 $("#submit").on("click",function(e){
 	e.preventDefault();
-	var length = $("#tbid tr").length;	//console.log(length);
-	if(length <=0){
-		alert('no input to submit');
-		e.preventDefault();
-		return false;
-	}
+var length = $("#tbid tr").length;	//console.log(length);
+if(length <=0){
+	alert('no input to submit');
+	e.preventDefault();
+	return false;
+}
 
-	var emptyarray = [];
+var emptyarray = [];
 
-	var i=0, j=0; var emptyname = 0, emptyemail = 0 , emptyrow = 0;
-	$("#tbid tr span").remove();
+var i=0, j=0; var emptyname = 0, emptyemail = 0 , emptyrow = 0;
+$("#tbid tr span").remove();
 
-	$.each($("#tbid tr"),function(){ 
-			var id = $(this).attr('id'); 
-			i++;
+$.each($("#tbid tr"),function(){ 
+	var id = $(this).attr('id'); 
+	i++;
 
-			console.log(id);
-			var inc = id.replace('row',''); //alert(inc);
-			 var name = "name"+inc.toString();
-			 var email = "email"+inc.toString();
-			  
-			var namevalue = $("#" +id).find("input[name = '"+name+"']").val();
-			var emailvalue = $("#" +id).find("input[name = '"+email+"']").val();
+	console.log(id);
+var inc = id.replace('row',''); //alert(inc);
+var name = "name"+inc.toString();
+var email = "email"+inc.toString();
 
-			// console.log(namevalue);
-			// console.log(emailvalue);
+var namevalue = $("#" +id).find("input[name = '"+name+"']").val();
+var emailvalue = $("#" +id).find("input[name = '"+email+"']").val();
 
-
-
-				if(namevalue == "" )
-				{
-
-									
-					emptyarray[i] = inc; //alert(inc);
-					
-					$('#'+id).find('input[name="'+name+'"]').after("<span name='emptyfield'> empty fields</span>");
-						emptyname++;
-					emptyarray[j] = i;
-					j++;
-
-				}
-
-				if(emailvalue == "" )
-				{
-
-									
-					emptyarray[i] = inc; //alert(inc);
-					
-					$('#'+id).find('input[name="'+email+'"]').after("<span name='emptyfield'> empty fields</span>");
-						emptyemail++;
-					emptyarray[j] = i;
-					j++;
-
-				}
-
-				if (namevalue == "" && emailvalue == "")
-				{
-
-					emptyrow++;
-					
-				}
-
-				
-	});
+// console.log(namevalue);
+// console.log(emailvalue);
 
 
 
-	var totalempty = (emptyname + emptyemail);
-	
-	$("#emptyinputfields").html('<br> Total empty rows are (<b> '+ emptyrow +' </b>) . <br/> Total empty name fields (<b> '+ emptyname +' </b>) . <br> Total empty email fields (<b>'+ emptyemail +'</b>) <br> Both name or email empty fields (<b>'+ totalempty +'</b>) ');
+if(namevalue == "" )
+{
+
+
+emptyarray[i] = inc; //alert(inc);
+
+$('#'+id).find('input[name="'+name+'"]').after("<span name='emptyfield'><b> empty fields</b></br></span>");
+emptyname++;
+emptyarray[j] = i;
+j++;
+
+}
+
+if(emailvalue == "" )
+{
+
+
+emptyarray[i] = inc; //alert(inc);
+
+$('#'+id).find('input[name="'+email+'"]').after("<span name='emptyfield'><b> empty fields</b></br></span>");
+emptyemail++;
+emptyarray[j] = i;
+j++;
+
+}
+
+if (namevalue == "" && emailvalue == "")
+{
+
+	emptyrow++;
+
+}
+
+
+});
 
 
 
-	if(totalempty  > 0){
-		console.log('empty');
-		return false;
-	}
+var totalempty = (emptyname + emptyemail);
+info = '<div class="alert alert-info remove"><a href="#" class="close" data-dismiss="alert" aria-label="close">&times;</a>Total empty rows are (<b> '+ emptyrow +' </b>).</div>';
+info += '<div class="alert alert-info remove"><a href="#" class="close" data-dismiss="alert" aria-label="close">&times;</a> Total empty name fields (<b> '+ emptyname +' </b>) .</div>';
+info +='<div class="alert alert-info remove"><a href="#" class="close" data-dismiss="alert" aria-label="close">&times;</a> Total empty email fields (<b>'+ emptyemail +'</b>).</div>';
+info +='<div class="alert alert-info remove"><a href="#" class="close" data-dismiss="alert" aria-label="close">&times;</a>Both name or email empty fields (<b>'+ totalempty +'</b>) </div>';
 
-	submitform(e);	
-	
+$("#emptyinputfields").html(info);
+
+
+
+if(totalempty  > 0){
+	console.log('empty');
+	return false;
+}
+
+submitform(e);	
+
 });
 
 
@@ -183,53 +261,27 @@ $("#submit").on("click",function(e){
 
 
 function submitform() {
-	
+
 	$.ajax({
 
-	url: 'selectalldata.php',
-	type: 'POST',
-	data: $('#formid').serialize(),
-	        //data: formData,
+		url: 'selectalldata.php',
+		type: 'POST',
+		data: $('#formid').serialize(),
+//data: formData,
 
-	success: function(result){
-		$("#emptyinputfields").append("<br>submitted");
-		setTimeout(function(){
-			// location = 'newfile.php';
-		},2000);
-	}
-	}); 
-		
+success: function(result){
+	$("#emptyinputfields").append("<br>submitted");
+	setTimeout(function(){
+// location = 'newfile.php';
+},2000);
+}
+}); 
+
 }
 
 });
 
 </script>
-</head>
-
-<body>
-<div id="outermost">
-	<button id="add">ADD</button>
-	<button id="delete">Delete</button>
-	<button id="fetch">Fetch</button>
-	<button id="clear">Clear</button>
-	rows:<span id="counter"></span>
-	<span id="emptyinputfields"></span>
-	
-<form id="formid" method="post" action="">
-	<table id="tabid" >
-		<tbody id="tbid" style="background-color:green;">
-		
-
-
-		</tbody>
-	</table>
-	<input type="submit" name="submit" id="submit">
-<!-- <div id="response"></div> -->
-	
-</form>
-
-</div>
-
 
 </body>
 </html>
